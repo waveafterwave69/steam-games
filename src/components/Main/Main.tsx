eb44b6ae368c827ca62ea88c76dd533aacc1e727
Michael Shonov <waveafterwave3w2@gmail.com>
2025-06-21 11:26:31 +0300

    promo

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,9 +1,31 @@
+import { Link } from 'react-router'
 import styles from './Main.module.css'
+
+import promoImg from '../../img/promoImg.png'
 
 const Main: React.FC = () => {
     return (
         <>
-            <main className={styles.main}></main>
+            <main className={styles.main}>
+                <div className={styles.main__row}>
+                    <div className={styles.row__text}>
+                        <h1 className={styles.text__title}>
+                            Вся информация о Steam играх.
+                        </h1>
+                        <h1 className={styles.text__description}>
+                            Источник с подробнной информацией о играх
+                        </h1>
+                        <Link to="/games" className={styles.text__btn}>
+                            Смотреть игры
+                        </Link>
+                    </div>
+                    <img
+                        src={promoImg}
+                        alt="steam"
+                        className={styles.main__img}
+                    />
+                </div>
+            </main>
         </>
     )
 }
